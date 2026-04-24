ccfcfff45e7c31dc4f34440c604f97b50e5efbff
rafaelafreitass <defreitasrafaela959@gmail.com>
2026-04-24 15:12:23 -0300

    meudeus

--- Assets/DestroyLixo.cs
+++ Assets/DestroyLixo.cs
@@ -13,11 +13,14 @@ public class DestroyLixo : MonoBehaviour
     {
         
     }
-
+public LixoSpawnerController lixoSpawnerController;
 private void OnCollisionEnter (Collision collision) {
 // Optional: Check if the entering object is the Player
 if (collision.gameObject.CompareTag("Lixo")) {
 Destroy(collision.gameObject);
+if(lixoSpawnerController.points > 0){
+lixoSpawnerController.AddToPoints(-1);
+}
 }
 }
 }
